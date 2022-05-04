6840851dbed3aad4dd55c33131d066459eae765c
JosephOlabode <josepholabode17@gmail.com>
2022-05-04 11:30:43 +0100

    created the repository user class

--- Repository/RepositoryBase.cs
+++ Repository/RepositoryBase.cs
@@ -10,11 +10,13 @@ namespace Repository
         protected RepositoryContext RepositoryContext;
         public RepositoryBase(RepositoryContext repositoryContext)
                 => RepositoryContext = repositoryContext;
+
         public IQueryable<T> FindAll(bool trackChanges) =>
                 !trackChanges ?
                   RepositoryContext.Set<T>()
                     .AsNoTracking() :
                   RepositoryContext.Set<T>();
+
         public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) =>
                 !trackChanges ?
                   RepositoryContext.Set<T>()
@@ -22,8 +24,11 @@ namespace Repository
                     .AsNoTracking() :
                   RepositoryContext.Set<T>()
                     .Where(expression);
+
         public void Create(T entity) => RepositoryContext.Set<T>().Add(entity);
+
         public void Update(T entity) => RepositoryContext.Set<T>().Update(entity);
+
         public void Delete(T entity) => RepositoryContext.Set<T>().Remove(entity);
     }
 }
