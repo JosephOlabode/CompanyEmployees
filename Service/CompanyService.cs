aa207213a83af36a26acba045ca0bbfbfe93a32d
JosephOlabode <josepholabode17@gmail.com>
2022-05-04 16:35:31 +0100

    created the service project

---
+++ Service/CompanyService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Contracts;
+
+namespace Service
+{
+	internal sealed class CompanyService
+	{
+		private readonly IRepositoryManager _repository;
+		private readonly ILoggerManager _logger;
+
+		public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+		{
+			_repository = repository;
+			_logger = logger;
+		}
+	}
+}
+
