42ed970be262e2b168a63d1b5d244684f42f219f
JosephOlabode <josepholabode17@gmail.com>
2022-05-05 12:30:34 +0100

    implemented the dto

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using Entities.Models;
+using Shared.DataTransferObjects;
 
 namespace Service.Contracts
 {
 	public interface ICompanyService
 	{
-		IEnumerable<Company> GetAllCompanies(bool trackChanges);
+		IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
 	}
 }
 
--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Contracts;
-using Entities.Models;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Service
 {
@@ -16,12 +16,15 @@ namespace Service
 			_logger = logger;
 		}
 
-        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
         {
             try
             {
                 var companies = _repository.Company.GetAllCompanies(trackChanges);
-                return companies;
+
+                var companiesDto = companies.Select(c => new CompanyDto(c.Id, c.Name ?? "", string.Join(' ', c.Address, c.Country))).ToList();
+
+                return companiesDto;
             } catch(Exception ex)
             {
                 _logger.LogError($"Something went wrong in the {nameof(GetAllCompanies)} service method {ex}");
