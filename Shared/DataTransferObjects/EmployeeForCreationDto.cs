0c0974f86fa46177715ad0b2ddc6e10ff21ca017
JosephOlabode <josepholabode17@gmail.com>
2022-05-21 13:23:02 +0100

    added validation to employee creation dto

--- Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -1,6 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Shared.DataTransferObjects
 {
-	public record EmployeeForCreationDto(string Name, int Age, string Position);
+	public record EmployeeForCreationDto(
+		[Required(ErrorMessage = "Employee name is a required field")]
+		[MaxLength(30, ErrorMessage = "Maximum length for name is 30 characters")]
+		string Name,
+
+		[Required(ErrorMessage ="Age is a required field")]
+		int Age,
+
+		[Required(ErrorMessage = "Position is a required field")]
+		[MaxLength(20, ErrorMessage = "Maximum length of position is 20 characters")]
+		string Position);
 }
 
