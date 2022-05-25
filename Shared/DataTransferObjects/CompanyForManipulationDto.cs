ffd2bceb93dd41865c319dc16ce7a4841d84a7ea
JosephOlabode <josepholabode17@gmail.com>
2022-05-25 11:49:11 +0100

    created and implemented the company manipulation dto

--- Shared/DataTransferObjects/CompanyForManipulationDto.cs
+++ Shared/DataTransferObjects/CompanyForManipulationDto.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Shared.DataTransferObjects
 {
-	public class CompanyForManipulationDto
+	public abstract record CompanyForManipulationDto
 	{
-		public CompanyForManipulationDto()
-		{
-		}
-	}
+        [Required(ErrorMessage = "Company name is a required field")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? Name { get; init; }
+
+        [Required(ErrorMessage = "Company address is a required field")]
+        [MaxLength(50, ErrorMessage = "Maximum length for the Name is 50 characters.")]
+        public string? Address { get; init; }
+
+        [Required(ErrorMessage = "Company country is a required field")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+        public string? Country { get; init; }
+
+        [Required(ErrorMessage = "Company country is a required field")]
+        IEnumerable<EmployeeForCreationDto>? Employees { get; init; }
+    }
 }
 
