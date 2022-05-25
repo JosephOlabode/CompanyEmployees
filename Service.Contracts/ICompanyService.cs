049cd00af669d6dbb80bbaf486c25c8f30f7b33b
JosephOlabode <josepholabode17@gmail.com>
2022-05-25 10:57:48 +0100

    made the company service interface asynchronous

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -5,13 +5,13 @@ namespace Service.Contracts
 {
 	public interface ICompanyService
 	{
-		IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
-		CompanyDto GetCompany(Guid companyId, bool trackChanges);
-		CompanyDto CreateCompany(CompanyForCreationDto company);
-		IEnumerable<CompanyDto> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
-		(IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection(IEnumerable<CompanyForCreationDto> companyCollection);
-		void DeleteCompany(Guid companyId, bool trackChanges);
-		void UpdateCompany(Guid companyId, CompanyForUpdateDto companyForUpdateDto, bool trackChanges);
+		Task<IEnumerable<CompanyDto>> GetAllCompanies(bool trackChanges);
+		Task<CompanyDto> GetCompany(Guid companyId, bool trackChanges);
+		Task<CompanyDto> CreateCompany(CompanyForCreationDto company);
+		Task<IEnumerable<CompanyDto>> GetByIds(IEnumerable<Guid> ids, bool trackChanges);
+		Task<(IEnumerable<CompanyDto> companies, string ids)> CreateCompanyCollection(IEnumerable<CompanyForCreationDto> companyCollection);
+		Task DeleteCompany(Guid companyId, bool trackChanges);
+		Task UpdateCompany(Guid companyId, CompanyForUpdateDto companyForUpdateDto, bool trackChanges);
 	}
 }
 
