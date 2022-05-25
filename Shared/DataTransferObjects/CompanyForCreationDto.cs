e964feda1b5febd3823255cfe73fdb8bc9eb0f70
JosephOlabode <josepholabode17@gmail.com>
2022-05-25 11:50:33 +0100

    implemented the company manipulation dto in create and update dtos

--- Shared/DataTransferObjects/CompanyForCreationDto.cs
+++ Shared/DataTransferObjects/CompanyForCreationDto.cs
@@ -3,22 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Shared.DataTransferObjects
 {
-	public record CompanyForCreationDto
-    {
-        [Required(ErrorMessage ="Company name is a required field")]
-        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
-        public string? Name { get; init; }
-
-        [Required(ErrorMessage ="Company address is a required field")]
-        [MaxLength(50, ErrorMessage = "Maximum length for the Name is 50 characters.")]
-        public string? Address { get; init; }
-
-        [Required(ErrorMessage = "Company country is a required field")]
-        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
-        public string? Country { get; init; }
-
-        [Required(ErrorMessage = "Company country is a required field")]
-        IEnumerable<EmployeeForCreationDto>? Employees { get; init; }
-    }
+	public record CompanyForCreationDto: CompanyForManipulationDto;
 }
 
--- Shared/DataTransferObjects/CompanyForUpdateDto.cs
+++ Shared/DataTransferObjects/CompanyForUpdateDto.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 namespace Shared.DataTransferObjects
 {
-	public record CompanyForUpdateDto(string Name, string Address, string Country, IEnumerable<EmployeeForUpdateDto> Employees);
+	public record CompanyForUpdateDto: CompanyForManipulationDto;
 }
 
