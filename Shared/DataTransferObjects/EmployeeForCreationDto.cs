84769f9914605444f96dd474761e0cfc4062eefc
JosephOlabode <josepholabode17@gmail.com>
2022-05-25 08:25:05 +0100

    formated the employee create dto to include validation

--- Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -3,16 +3,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Shared.DataTransferObjects
 {
-	public record EmployeeForCreationDto(
+	public record EmployeeForCreationDto {
 		[Required(ErrorMessage = "Employee name is a required field")]
 		[MaxLength(30, ErrorMessage = "Maximum length for name is 30 characters")]
-		string Name,
+		public string? Name { get; init; }
 
-		[Required(ErrorMessage ="Age is a required field")]
-		int Age,
+		[Required(ErrorMessage = "Age is a required field")]
+		public int Age { get; init; }
 
 		[Required(ErrorMessage = "Position is a required field")]
 		[MaxLength(20, ErrorMessage = "Maximum length of position is 20 characters")]
-		string Position);
+		public string? Position { get; init; }
+	}
 }
 
