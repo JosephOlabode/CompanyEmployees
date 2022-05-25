ca8174cc30d8cf5fad697b18de26db0b0ce8705f
JosephOlabode <josepholabode17@gmail.com>
2022-05-25 09:13:57 +0100

    implemented the employee manipulation dto in create and update employee

--- Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -3,17 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Shared.DataTransferObjects
 {
-	public record EmployeeForCreationDto {
-		[Required(ErrorMessage = "Employee name is a required field")]
-		[MaxLength(30, ErrorMessage = "Maximum length for name is 30 characters")]
-		public string? Name { get; init; }
-
-		[Range(18, int.MaxValue, ErrorMessage = "Age is required and it can't be lower than 18")]
-		public int Age { get; set; }
-
-		[Required(ErrorMessage = "Position is a required field")]
-		[MaxLength(20, ErrorMessage = "Maximum length of position is 20 characters")]
-		public string? Position { get; init; }
-	}
+	public record EmployeeForCreationDto: EmployeeForManipulationDto;
 }
 
