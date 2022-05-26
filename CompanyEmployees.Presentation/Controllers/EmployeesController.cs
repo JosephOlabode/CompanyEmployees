d81ae6c7cd08d77b5179c6fbcfb7c58c378b2e48
JosephOlabode <josepholabode17@gmail.com>
2022-05-26 13:29:40 +0100

    added the query parameter to the service call in the controller

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -17,7 +17,7 @@ namespace CompanyEmployees.Presentation.Controllers
 		[HttpGet]
 		public async Task<IActionResult> GetEmployeesForCompany(Guid companyId, [FromQuery]EmployeeParameters employeeParameters)
 		{
-			var employees = await _service.EmployeeService.GetEmployeesAsync(companyId, trackChanges: false);
+			var employees = await _service.EmployeeService.GetEmployeesAsync(companyId, employeeParameters, trackChanges: false);
 
 			return Ok(employees);
 		}
