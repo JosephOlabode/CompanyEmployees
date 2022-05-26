f36a8f5d63f5d64e3f3593700dfcdaa1d4c37803
JosephOlabode <josepholabode17@gmail.com>
2022-05-26 15:18:04 +0100

    implemented the pagination metadata on service level

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -66,14 +66,14 @@ namespace Service
 			return (employeeToPatch, employeeEntity);
         }
 
-        public async Task<IEnumerable<EmployeeDto>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
+        public async Task<(IEnumerable<EmployeeDto> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
 
 			var company = await CheckIfCompanyExist(companyId, trackChanges);
 
-			var employeesFromDb = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges);
-			var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
-			return employeesDto;
+			var employeesWithMetaData = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges);
+			var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesWithMetaData);
+			return (employees: employeesDto, metaData: employeesWithMetaData.MetaData);
 		}
 
         public async Task SaveChangesForPatchAsync(EmployeeForUpdateDto employeeToPatch, Employee employeeEntity)
