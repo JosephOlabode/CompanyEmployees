5047692846de66e9b2edde0ae71b9bb1526dfbc4
JosephOlabode <josepholabode17@gmail.com>
2022-05-26 13:28:15 +0100

    added the implementation to the employee service

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -5,6 +5,7 @@ using Entities.Exceptions;
 using Entities.Models;
 using Service.Contracts;
 using Shared.DataTransferObjects;
+using Shared.RequestFeatures;
 
 namespace Service
 {
@@ -65,12 +66,12 @@ namespace Service
 			return (employeeToPatch, employeeEntity);
         }
 
-        public async Task<IEnumerable<EmployeeDto>> GetEmployeesAsync(Guid companyId, bool trackChanges)
+        public async Task<IEnumerable<EmployeeDto>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
 
 			var company = await CheckIfCompanyExist(companyId, trackChanges);
 
-			var employeesFromDb = await _repository.Employee.GetEmployeesAsync(companyId, trackChanges);
+			var employeesFromDb = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges);
 			var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
 			return employeesDto;
 		}
