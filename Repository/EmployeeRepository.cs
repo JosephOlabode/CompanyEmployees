bdd7184a3704b23fdb4a426ec1b2ccb4453b06e6
JosephOlabode <josepholabode17@gmail.com>
2022-05-26 13:24:36 +0100

    implemented the logic of pagination in getemployees method

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -2,6 +2,7 @@
 using Contracts;
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Shared.RequestFeatures;
 
 namespace Repository
 {
@@ -12,9 +13,11 @@ namespace Repository
 		{
 		}
 
-		public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges) =>
+		public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges) =>
 			await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
 			.OrderBy(e => e.Name)
+			.Skip((employeeParameters.pageNumber - 1) * employeeParameters.PageSize)
+			.Take(employeeParameters.PageSize)
 			.ToListAsync();
 
         public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
