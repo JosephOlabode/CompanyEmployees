5ad9abe5cd3c72c3d6f5e22dee012e5d96abf4d5
JosephOlabode <josepholabode17@gmail.com>
2022-06-01 09:06:25 +0100

    extended the employee parameters

--- Shared/RequestFeatures/EmployeeParameters.cs
+++ Shared/RequestFeatures/EmployeeParameters.cs
@@ -2,7 +2,11 @@
 namespace Shared.RequestFeatures
 {
 	public class EmployeeParameters: RequestParameters
-	{ 
+	{
+		public uint MinAge { get; set; }
+		public uint MaxAge { get; set; } = int.MaxValue;
+
+		public bool ValidAgeRange => MaxAge > MinAge;
 	}
 }
 
