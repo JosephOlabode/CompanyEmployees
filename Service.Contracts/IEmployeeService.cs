4fa3546350cde1c9d8c05e04342c9e033046f517
JosephOlabode <josepholabode17@gmail.com>
2022-06-03 03:18:51 +0100

    modified the get employee async interface

--- Service.Contracts/IEmployeeService.cs
+++ Service.Contracts/IEmployeeService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Dynamic;
+using Entities.LinkModels;
 using Entities.Models;
 using Shared.DataTransferObjects;
 using Shared.RequestFeatures;
@@ -8,7 +9,7 @@ namespace Service.Contracts
 {
 	public interface IEmployeeService
 	{
-		Task<(IEnumerable<ExpandoObject> employees, MetaData metaData)> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges);
+		Task<(LinkResponse linkResponse, MetaData metaData)> GetEmployeesAsync(Guid companyId, LinkParameters linkParameters, bool trackChanges);
 		Task<EmployeeDto> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges);
 		Task<EmployeeDto> CreateEmployeeForCompanyAsync (Guid companyId, EmployeeForCreationDto employeeForCreation, bool trackChanges);
 		Task DeleteEmployeeForCompanyAsync(Guid companyId, Guid id, bool trackChanges);
