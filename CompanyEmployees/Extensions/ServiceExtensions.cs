59e9405a53161bfe11b1fd00d30181b943d5f511
JosephOlabode <josepholabode17@gmail.com>
2022-06-03 02:11:47 +0100

    implemented the AddCustomMediaTypes method

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Contracts;
 using LoggerService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Repository;
 using Service;
@@ -44,6 +46,27 @@ namespace CompanyEmployees.Extensions
             services.AddScoped<IServiceManager, ServiceManager>();
 
         public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) => builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
+
+        public static void AddCustomMediaTypes(this IServiceCollection services)
+        {
+            services.Configure<MvcOptions>(config => {
+                var systemTextJsonOutputFormatter = config.OutputFormatters.OfType<SystemTextJsonOutputFormatter>()?.FirstOrDefault();
+
+                if (systemTextJsonOutputFormatter != null)
+                {
+                    systemTextJsonOutputFormatter.SupportedMediaTypes.Add("application/vnd.codemaze.hateoas+json");
+                }
+
+                var xmlOutputFormatter = config.OutputFormatters
+                          .OfType<XmlDataContractSerializerOutputFormatter>()?
+                          .FirstOrDefault();
+
+                if (xmlOutputFormatter != null)
+                {
+                    xmlOutputFormatter.SupportedMediaTypes.Add("application/vnd.codemaze.hateoas+xml");
+                }
+            });
+        }
     }
 }
 
