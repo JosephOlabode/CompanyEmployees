0261c2c137b06f5f18fcfb3a19807ca640a7cb07
JosephOlabode <josepholabode17@gmail.com>
2022-06-03 02:21:38 +0100

    implemented the on executing media function

--- CompanyEmployees.Presentation/ActionFilters/ValidationFilterAttribute.cs
+++ CompanyEmployees.Presentation/ActionFilters/ValidationFilterAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -17,18 +18,37 @@ namespace CompanyEmployees.ActionFilters
 
         public void OnActionExecuting(ActionExecutingContext context)
         {
-            var action = context.RouteData.Values["action"];
-            var controller = context.RouteData.Values["controller"];
+            //var action = context.RouteData.Values["action"];
+            //var controller = context.RouteData.Values["controller"];
 
-            var param = context.ActionArguments.SingleOrDefault(x => x.Value.ToString().Contains("Dto")).Value;
-            if(param is null)
+            //var param = context.ActionArguments.SingleOrDefault(x => x.Value.ToString().Contains("Dto")).Value;
+            //if(param is null)
+            //{
+            //    context.Result = new BadRequestObjectResult($"Object is null. Controller:{ controller }, action: { action}");
+            //    return;
+            //}
+
+            //if (!context.ModelState.IsValid)
+            //    context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+
+            var acceptHeaderPresent = context.HttpContext
+                     .Request.Headers.ContainsKey("Accept");
+
+            if (!acceptHeaderPresent)
             {
-                context.Result = new BadRequestObjectResult($"Object is null. Controller:{ controller }, action: { action}");
+                context.Result = new BadRequestObjectResult($"Accept header is missing.");
                 return;
             }
 
-            if (!context.ModelState.IsValid)
-                context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+            var mediaType = context.HttpContext.Request.Headers["Accept"].FirstOrDefault();
+
+            if (!MediaTypeHeaderValue.TryParse(mediaType, out MediaTypeHeaderValue ? outMediaType))
+            {
+                context.Result = new BadRequestObjectResult($"Media type not present. Please add Accept header with the required media type.");
+                return;
+            }
+            context.HttpContext.Items.Add("AcceptHeaderMediaType", outMediaType);
+
         }
     }
 }
