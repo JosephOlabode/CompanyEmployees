2dabb50bed212141c9d6f0f272631e19534644fd
JosephOlabode <josepholabode17@gmail.com>
2022-06-03 02:34:23 +0100

    implemented the employee link class

--- CompanyEmployees/Utility/EmployeeLinks.cs
+++ CompanyEmployees/Utility/EmployeeLinks.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using Contracts;
+using Shared.DataTransferObjects;
+
 namespace CompanyEmployees.Utility
 {
-	public class EmployeeLinks
-	{
-		public EmployeeLinks()
-		{
-		}
-	}
+	public class EmployeeLinks: IEmployeeLinks
+    {
+        private readonly LinkGenerator _linkGenerator;
+        private readonly IDataShaper<EmployeeDto> _dataShaper;
+        public EmployeeLinks(LinkGenerator linkGenerator, IDataShaper<EmployeeDto> dataShaper)
+        {
+            _linkGenerator = linkGenerator;
+            _dataShaper = dataShaper;
+        }
+    }
 }
 
