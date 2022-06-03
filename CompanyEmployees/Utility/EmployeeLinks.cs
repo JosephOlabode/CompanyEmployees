648e82d870c07bf7d4e78152b6f61adeb88bb6aa
JosephOlabode <josepholabode17@gmail.com>
2022-06-03 02:49:01 +0100

    implemented the first overload of create link employee

--- CompanyEmployees/Utility/EmployeeLinks.cs
+++ CompanyEmployees/Utility/EmployeeLinks.cs
@@ -54,6 +54,29 @@ namespace CompanyEmployees.Utility
             var linkedEmployees = CreateLinksForEmployees(httpContext, employeeCollection);
             return new LinkResponse { HasLinks = true, LinkedEntities = linkedEmployees };
         }
+
+        private List<Link> CreateLinksForEmployee(HttpContext httpContext, Guid companyId, Guid id, string fields = "")
+        {
+            var links = new List<Link>
+            {
+                new Link(_linkGenerator.GetUriByAction(httpContext, "GetEmployeeForCompany", values: new { companyId, id, fields }),
+                "self","GET"),
+                new Link(_linkGenerator.GetUriByAction(httpContext,
+                "DeleteEmployeeForCompany", values: new { companyId, id }),
+                        "delete_employee",
+                "DELETE"),
+                new Link(_linkGenerator.GetUriByAction(httpContext,
+                "UpdateEmployeeForCompany", values: new { companyId, id }),
+                        "update_employee",
+                        "PUT"),
+                new Link(_linkGenerator.GetUriByAction(httpContext,
+                "PartiallyUpdateEmployeeForCompany", values: new { companyId, id }),
+                        "partially_update_employee",
+                "PATCH")
+            };
+
+            return links;
+        }
     }
 }
 
