26f233863019eb804ea903babc91789ad1972bd9
JosephOlabode <josepholabode17@gmail.com>
2022-06-04 02:33:05 +0100

    implemented the get root method

--- CompanyEmployees.Presentation/Controllers/RootController.cs
+++ CompanyEmployees.Presentation/Controllers/RootController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Entities.LinkModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
@@ -10,6 +12,43 @@ namespace CompanyEmployees.Presentation
 	{
 		private readonly LinkGenerator _linkGenerator;
 		public RootController(LinkGenerator linkGenerator) => _linkGenerator = linkGenerator;
+
+		[HttpGet(Name = "GetRoot")]
+		public IActionResult GetRoot([FromHeader(Name = "Accept")] string mediaType)
+        {
+			if (mediaType.Contains("application/vnd.codemaze.apiroot"))
+            {
+				var list = new List<Link>
+				{
+					new Link
+					{
+						Href = _linkGenerator.GetUriByName(HttpContext, nameof(GetRoot), new{ }),
+						Rel = "self",
+						Method = "GET"
+					},
+
+					new Link
+                    {
+						Href = _linkGenerator.GetUriByName(HttpContext, "GetCompanies", new{ }),
+						Rel = "companies",
+						Method = "GET"
+
+					},
+					new Link
+                    {
+						Href = _linkGenerator.GetUriByName(HttpContext, "CreateCompany", new{ }),
+						Rel = "create_company",
+						Method = "POST"
+					}
+				};
+
+				return Ok(list);
+
+			}
+
+			return NoContent();
+
+		}
 	}
 }
 
