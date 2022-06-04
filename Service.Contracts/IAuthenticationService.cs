02891b4eb1ca21c360a59fb0d1e30d0d97fe17ab
JosephOlabode <josepholabode17@gmail.com>
2022-06-04 15:07:04 +0100

    created i authentication service interface

---
+++ Service.Contracts/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Shared.DataTransferObjects;
+
+namespace Service.Contracts
+{
+	public interface IAuthenticationService
+	{
+		Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration);
+	}
+}
+
