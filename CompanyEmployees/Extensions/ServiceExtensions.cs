652d355433e83a9176381085de2ffaded30bdc27
JosephOlabode <josepholabode17@gmail.com>
2022-06-04 09:12:41 +0100

    added the configuration for max age

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -2,6 +2,7 @@
 using CompanyEmployees.Presentation.Controllers;
 using Contracts;
 using LoggerService;
+using Marvin.Cache.Headers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.Versioning;
@@ -88,7 +89,17 @@ namespace CompanyEmployees.Extensions
         public static void ConfigureResponseCaching(this IServiceCollection services) => services.AddResponseCaching();
 
         public static void ConfigureHttpCacheHeaders(this IServiceCollection services) =>
-            services.AddHttpCacheHeaders();
+            services.AddHttpCacheHeaders(
+
+                (expirationOpt) =>
+                {
+                    expirationOpt.MaxAge = 65;
+                    expirationOpt.CacheLocation = CacheLocation.Private;
+                },
+                (validationOpt) =>
+                {
+                    validationOpt.MustRevalidate = true;
+                });
     }
 }
 
