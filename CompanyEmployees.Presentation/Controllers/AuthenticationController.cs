f64ad945ee4f339d8ca552c0a37b220233c699fa
JosephOlabode <josepholabode17@gmail.com>
2022-06-04 15:22:52 +0100

    implemented the register user controller

--- CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
+++ CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using CompanyEmployees.ActionFilters;
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace CompanyEmployees.Presentation.Controllers
 {
@@ -8,6 +10,24 @@ namespace CompanyEmployees.Presentation.Controllers
 	{
 		private readonly IServiceManager _service;
 		public AuthenticationController(IServiceManager service) => _service = service;
+
+		[HttpPost]
+		[ServiceFilter(typeof(ValidationFilterAttribute))]
+		public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+			var result = await _service.AuthenticationService.RegisterUser(userForRegistration);
+
+			if (!result.Succeeded)
+            {
+				foreach(var error in result.Errors)
+                {
+					ModelState.TryAddModelError(error.Code, error.Description);
+				}
+				return BadRequest(ModelState);
+			}
+
+			return StatusCode(201);
+		}
 	}
 }
 
