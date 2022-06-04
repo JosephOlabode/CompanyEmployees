b85d3d2abae8c50362ce893cc92dc24da068bf94
JosephOlabode <josepholabode17@gmail.com>
2022-06-04 02:13:56 +0100

    added the root controller document

---
+++ CompanyEmployees.Presentation/Controllers/RootController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace CompanyEmployees.Presentation
+{
+	[Route("api")]
+	[ApiController]
+	public class RootController
+	{
+		private readonly LinkGenerator _linkGenerator;
+		public RootController(LinkGenerator linkGenerator) => _linkGenerator = linkGenerator;
+	}
+}
+
