13f5cc1e273e0f0cc648239a671ad2161b5bd304
JosephOlabode <josepholabode17@gmail.com>
2022-06-04 02:46:09 +0100

    configuring api versioning

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -69,6 +69,16 @@ namespace CompanyEmployees.Extensions
                 }
             });
         }
+
+        public static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(opt =>
+            {
+                opt.ReportApiVersions = true;
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+        }
     }
 }
 
