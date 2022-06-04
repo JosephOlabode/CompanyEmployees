841632ca5f4c8acb0aaadf8ba7b82570ddba3c88
JosephOlabode <josepholabode17@gmail.com>
2022-06-04 14:55:30 +0100

    created and implemented the authentication controller

---
+++ CompanyEmployees.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace CompanyEmployees.Presentation.Controllers
+{
+	public class AuthenticationController: ControllerBase
+	{
+		private readonly IServiceManager _service;
+		public AuthenticationController(IServiceManager service) => _service = service;
+	}
+}
+
